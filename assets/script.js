ad6817fa7043c3d9aa09ce1b7bd3b9d6e8177df5
Your Name <matt14w@gmail.com>
2021-12-12 19:02:34 -0500

    weather updated

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,21 @@
 var currentForecast = document.getElementById("current");
 var key = "ca90b1ee3ee78f78b6f96f9d4ec5800a"  
 var fiveDayForecast = document.getElementById("forecast");
-var favorite = document.getElementById("fav-btn");
-
-
+var favorite = document.getElementById("saved");
+var container = document.getElementById('container');
 
 
 
 function getForecast() {
-   document.getElementById("btn").onclick = function(event) {
-       event.preventDefault();
-   var location = document.getElementById("location").value;
-   localStorage.setItem("fav-city", location)
-    console.log(location)
+document.getElementById("btn").onclick = function(event) {
+  event.preventDefault();
+  
+  
+  var location = document.getElementById("location").value;
+  localStorage.setItem("fav-city", JSON.stringify(location));
+  console.log(location)
 
-    var getLocal = document.createElement("button")
-    getLocal.textContent = localStorage.getItem("fav-city")
-    
-    favorite.appendChild(getLocal)
+
     
 
     //Getting latitude and longitute
@@ -30,9 +28,6 @@ function getForecast() {
         })
         .then (function(data){
 
-            //Console Logging for testing
-            console.log("lat " + data.coord.lat)
-            console.log("lon " + data.coord.lon);
 
             var lat = data.coord.lat;
             var lon = data.coord.lon;
@@ -45,40 +40,30 @@ function getForecast() {
                 
             })
             .then(function(response){
-                console.log("Current " + response.current.temp + "degrees");
-                console.log ("wind " + response.current.wind_speed);
-                console.log("Humidity " + response.current.humidity + "%")
-                console.log("UVI " + response.current.uvi);
-
-                var city = document.createElement("h3");
-                city.textContent= location
-
-                var currentTemp = document.createElement("h4");
-                currentTemp.textContent = "Current Temp: " + parseInt(response.current.temp) + "° F"
-
-                var windSpeed = document.createElement("p");
-                windSpeed.textContent = "Wind: " + parseInt(response.current.wind_speed) + " MPH";
-
-                var humidity = document.createElement("p");
-                humidity.textContent = "Humidity: " + response.current.humidity + " %";
-
-                var uvi = document.createElement("p");
-                uvi.textContent = "UV Index: " + response.current.uvi;
 
 
+                var currentDate = (response.daily[0].dt);
+                    var todayDate = new Date(0);
+                    todayDate.setUTCSeconds(currentDate);
+                    var date = todayDate;
+                    var month = date.getMonth()+1;
+                    var day = date.getDate();
+                    var year = date.getFullYear();
+
+                    var dayOutput  = month + "/" + day + "/" + year
 
 
-                
-                currentForecast.append(city);
-                currentForecast.append(currentTemp);
-                currentForecast.append(windSpeed);
-                currentForecast.append(humidity);
-                currentForecast.append(uvi);
-              
+                document.getElementById("local").innerHTML = location + "-" + dayOutput
+                document.getElementById('localtemp').innerHTML = "Current Temp: " + parseInt(response.current.temp) + "° F"
+                document.getElementById("localwind").innerHTML = "Wind: " + parseInt(response.current.wind_speed) + " MPH";
+                document.getElementById("localhumidity").innerHTML = "Humidity: " + response.current.humidity + " %";
+                document.getElementById("localuv").innerHTML = "UV Index: " + response.current.uvi;
                 
                 
-                
-                for (var i=1; i<6; i++){
+                function getFiveDay(){
+
+
+                    for (var i=1; i<6; i++){
                     
                     var futureDateResponse = (response.daily[i].dt);
                     var forecastDate = new Date(i);
@@ -88,13 +73,13 @@ function getForecast() {
                     var forecastDay = futureDate.getDate();
                     var forecastYear = futureDate.getFullYear();
 
-                    console.log(forecastMonth + "/" + forecastDay + "/" + forecastYear)
 
                     var dateOutput = forecastMonth + "/" + forecastDay + "/" + forecastYear
 
                    
                     var forecast = document.createElement("div")
                     forecast.classList.add("card")
+                    forecast.setAttribute("id", "cards")
                     forecast.textContent = dateOutput
                   
 
@@ -114,36 +99,28 @@ function getForecast() {
                     forecast.append(forecastTemp);
                     forecast.append(forecastWind);
                     forecast.append(forecastHumidity)
-
-                    
-
-                    console.log(fiveDayForecast);
-
                 }
 
                 
-            })
-       
-        })
-}
+               
+            }
 
-}
+ getFiveDay();
+               
+                })
 
+                })
 
-
-    getForecast();
-
-
-
-
+            }
+            
+        }
 
 
 
 
 
-//for (var i=0; i < 6; i++){
-   // var div = document.createElement("div")
-   // div.innerHTML = '<div class="card" id="card"></div>'
-    //div.appendChild(fiveDayForecast);
-    //console.log(div)
-//}
+
+
+
+getForecast();
+   
